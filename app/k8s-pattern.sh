da1e88e645308995de1fc75b200c87d9211dd636
Joe Colandro <jcolandro@macbookjoe.local>
2021-05-11 22:31:25 -0400

    kubernetes examples

--- app/k8s-pattern.sh
+++ app/k8s-pattern.sh
@@ -1,3 +1,6 @@
+#set kubectl from terraform output of aks cluster
+az aks get-credentials --resource-group $(terraform output -raw resource_group_name) --name $(terraform output -raw kubernetes_cluster_name)
+
 vault secrets enable -path secret kv-v2
 vault kv put secret/app1/config username="demo" password="IT_WORKED"
 
@@ -9,6 +12,7 @@ helm repo add hashicorp https://helm.releases.hashicorp.com
 helm install vault hashicorp/vault \
     --set "injector.externalVaultAddr=$VAULT_ADDR"
 
+
 # determine vault service account JWT token
 VAULT_HELM_SECRET_NAME=$(kubectl get secrets --output=json | jq -r '.items[].metadata | select(.name|startswith("vault-token-")).name')
 kubectl describe secret $VAULT_HELM_SECRET_NAME
@@ -16,12 +20,15 @@ kubectl describe secret $VAULT_HELM_SECRET_NAME
 TOKEN_REVIEW_JWT=$(kubectl get secret $VAULT_HELM_SECRET_NAME --output='go-template={{ .data.token }}' | base64 --decode)
 KUBE_CA_CERT=$(kubectl config view --raw --minify --flatten --output='jsonpath={.clusters[].cluster.certificate-authority-data}' | base64 --decode)
 KUBE_HOST=$(kubectl config view --raw --minify --flatten --output='jsonpath={.clusters[].cluster.server}')
+KUBE_ISSUER=${KUBE_HOST%%:443} 
 
 # configure kubernetes auth method
 vault write auth/kubernetes/config \
+        issuer=""$KUBE_ISSUER"" \
         token_reviewer_jwt="$TOKEN_REVIEW_JWT" \
         kubernetes_host="$KUBE_HOST" \
-        kubernetes_ca_cert="$KUBE_CA_CERT"
+        kubernetes_ca_cert="$KUBE_CA_CERT" \
+        disable_iss_validation="true"
 
 # create acl policy to allow accress to secret
 vault policy write app1 - <<EOF
@@ -45,12 +52,12 @@ kubectl exec \
     $(kubectl get pod -l app=app1 -o jsonpath="{.items[0].metadata.name}") \
     --container app1 -- cat /vault/secrets/app1-config.txt ; echo
 
-kubectl exec \
-    $(kubectl get pod -l app=app1 -o jsonpath="{.items[0].metadata.name}") \
-    --container vault-agent-init -- cat /home/vault/config.json ; echo
+# kubectl exec \
+#     $(kubectl get pod -l app=app1 -o jsonpath="{.items[0].metadata.name}") \
+#     --container vault-agent-init -- cat /home/vault/config.json ; echo
 
 
-# env variable example
+######### env variable example
 
 # create app2 vault role
 vault write auth/kubernetes/role/app2 \
@@ -63,10 +70,62 @@ vault write auth/kubernetes/role/app2 \
 kubectl apply -f service-account-app2.yml
 kubectl apply -f env-deployment.yml
 
-kubectl exec \
-    $(kubectl get pod -l app=app2 -o jsonpath="{.items[0].metadata.name}") \
-    --container app2 -- printenv RESULT
 
-kubectl exec -it $(kubectl get pod -l app=app2 -o jsonpath="{.items[0].metadata.name}") --container app2 -- /bin/sh
+# kubectl exec -it $(kubectl get pod -l app=app2 -o jsonpath="{.items[0].metadata.name}") --container app2 -- /bin/sh
 
+# 'env' was executed by container init script and printed to logs. this command views the container log to see that the environment variable was set.
 kubectl logs $(kubectl get pod -l app=app2 -o jsonpath="{.items[0].metadata.name}") app2
+
+
+############ CSI ################
+
+helm install vault-csi hashicorp/vault \
+    --set "csi.enabled=true" \
+    --set "injector.enabled=false" \
+    --set "server.enabled=false" 
+
+helm repo add secrets-store-csi-driver https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/master/charts
+helm install csi secrets-store-csi-driver/secrets-store-csi-driver
+
+#create vault role in kubernetes auth method for CSI app
+vault write auth/kubernetes/role/csi-app \
+    bound_service_account_names=csi \
+    bound_service_account_namespaces=default \
+    policies=app1 \
+    ttl=20m
+
+
+# apply the secret provider class
+kubectl apply -f spc-vault-database.yml
+kubectl describe SecretProviderClass vault-database
+
+# deploy the service account app3 to use the csi driver
+kubectl apply -f service-account-csi.yml
+kubectl apply -f csi-deployment.yml
+
+#display the password secret written to the file system at /mnt/secrets-store/db-password
+kubectl exec \
+    $(kubectl get pod -l app=app3 -o jsonpath="{.items[0].metadata.name}") \
+    --container app3 -- cat /mnt/secrets-store/db-password ; echo
+
+kubectl exec \
+    $(kubectl get pod -l app=app3 -o jsonpath="{.items[0].metadata.name}") \
+    --container app3 -- printenv
+
+
+# dynamic postgres credential example
+
+# create the role for app1
+vault write auth/kubernetes/role/app4 \
+        bound_service_account_names=app4 \
+        bound_service_account_namespaces=default \
+        policies=app1 \
+        ttl=24h
+
+kubectl apply -f service-account-app4.yml
+kubectl apply -f dyn-file-deployment.yml 
+
+kubectl exec \
+    $(kubectl get pod -l app=app4 -o jsonpath="{.items[0].metadata.name}") \
+    --container app4 -- cat /vault/secrets/app4-config.txt ; echo
+
---
+++ app/postgres/postgres.sh
@@ -0,0 +1,42 @@
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+
+helm install postgres bitnami/postgresql \
+  --set service.type=LoadBalancer \
+    
+export POSTGRES_PASSWORD=$(kubectl get secret --namespace default postgres-postgresql -o jsonpath="{.data.postgresql-password}" | base64 --decode)
+
+kubectl run postgres-postgresql-client --rm --tty -i --restart='Never' --namespace default --image docker.io/bitnami/postgresql:11.11.0-debian-10-r86 --env="PGPASSWORD=$POSTGRES_PASSWORD" --command -- psql --host postgres-postgresql -U postgres -d postgres -p 5432
+
+export SERVICE_IP=$(kubectl get svc --namespace default postgres-postgresql --template "{{ range (index .status.loadBalancer.ingress 0) }}{{.}}{{ end }}")
+    
+PGPASSWORD="$POSTGRES_PASSWORD" psql --host $SERVICE_IP --port 5432 -U postgres -d postgres
+
+CREATE ROLE ro NOINHERIT;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO "ro";
+\q
+
+vault secrets enable database
+
+vault write database/config/postgresql \
+     plugin_name=postgresql-database-plugin \
+     connection_url="postgresql://{{username}}:{{password}}@$SERVICE_IP:5432/postgres?sslmode=disable" \
+     allowed_roles=readonly \
+     username="postgres" \
+     password="$POSTGRES_PASSWORD"
+
+tee readonly.sql <<EOF
+CREATE ROLE "{{name}}" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}' INHERIT;
+GRANT ro TO "{{name}}";
+EOF
+
+vault write database/roles/readonly \
+      db_name=postgresql \
+      creation_statements=@readonly.sql \
+      default_ttl=1h \
+      max_ttl=24h
+
+
+PGPASSWORD="$POSTGRES_PASSWORD" psql --host $SERVICE_IP --port 5432 -U postgres -d postgres
+
+SELECT usename, valuntil FROM pg_user;
